f5fbf6bc4c2555026e04a0fface36871b6fcae6e
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-06-20 13:14:30 +0300

    Added DML and DQL queries

--- src/main/resources/db.migration/V1__initial_schema.sql
+++ src/main/resources/db.migration/V1__initial_schema.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA cutlink;
-
 CREATE TABLE user_link (
     id BIGSERIAL PRIMARY KEY,
     url TEXT NOT NULL UNIQUE
@@ -7,6 +5,25 @@ CREATE TABLE user_link (
 
 CREATE TABLE generate_link (
     id BIGSERIAL PRIMARY KEY,
-    generate_url TEXT NOT NULL UNIQUE,
+    url TEXT NOT NULL UNIQUE,
     user_link_id INT REFERENCES user_link(id) ON DELETE CASCADE
 );
+
+INSERT INTO user_link(url)
+VALUES ('https://google.com');
+
+INSERT INTO generate_link (url, user_link_id)
+VALUES ('test', 1);
+
+SELECT id,
+       url,
+       user_link_id
+FROM generate_link
+WHERE user_link_id = ?;
+
+SELECT generate_link.id,
+       generate_link.url,
+       user_link_id
+FROM generate_link
+JOIN user_link ul on generate_link.user_link_id = ul.id
+WHERE ul.url = 'https://google.com';
